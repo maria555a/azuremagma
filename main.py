00a3178e5b542384e943bfc63cea56828c364f6f
Maria <theweirdrider@gmail.com>
2025-04-21 18:05:49 +0300

    Added code for printing digits of a 4-digit number

---
+++ main.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a 4-digit number: "))
+thousands = num // 1000
+hundreds = (num % 1000) // 100
+tens = (num % 100) // 10
+ones = num % 10
+
+print(thousands)
+print(hundreds)
+print(tens)
+print(ones)
