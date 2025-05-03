7da64d96c555c57ab4b75a96526ea49f8e18db88
Maria <theweirdrider@gmail.com>
2025-05-03 16:32:44 +0300

    divide.list

--- divide.list.py
+++ divide.list.py
@@ -5,17 +5,17 @@ if len(lst) == 0:
 
 
 lst = [9, 6, 3, 9, 8, 9, 6, 9]
-mid = len(lst) // 2
-lst1 = lst[:mid]
-lst2 = lst[mid:]
+part = len(lst) // 2
+lst1 = lst[:part]
+lst2 = lst[part:]
 result = [lst1, lst2]
 print(result)
 
 
 lst = [3, 5, 2, 5, 7]
-mid = (len(lst) + 1) // 2
-lst1 = lst[:mid]
-lst2 = lst[mid:]
+part = (len(lst) + 1) // 2
+lst1 = lst[:part]
+lst2 = lst[part:]
 result = [lst1, lst2]
 print(result)
 
