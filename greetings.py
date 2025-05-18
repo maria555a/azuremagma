8ec99abd107d5b128c18613ab007bd434d2dbab5
Maria <theweirdrider@gmail.com>
2025-05-18 14:33:46 +0300

    greetings

---
+++ greetings.py
@@ -0,0 +1,7 @@
+def say_hi(your_name, your_age):
+    return f"Hi. My name is {your_name} and I am {your_age} years old"
+your_name = input("Enter name: ")
+your_age = int(input("Enter age: "))
+
+print(say_hi(your_name, your_age))
+
